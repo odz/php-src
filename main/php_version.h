e22df5b85d4c99c1e3eca668519c93937d346130
Akira Oda <oda.org@gmail.com>
2020-06-30 02:00:06 +0900

    php-7.1.29

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 28
+#define PHP_RELEASE_VERSION 29
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "7.1.28"
-#define PHP_VERSION_ID 70128
+#define PHP_VERSION "7.1.29"
+#define PHP_VERSION_ID 70129
